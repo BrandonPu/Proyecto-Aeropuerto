436e67b062ffeceb3ec7495cca92bb6403b3454b
Informatica <informatica@alumno.local>
2024-09-26 17:22:18 -0600

    creación de vista en terminal

--- src/main/java/com/aeropuerto/webapp/Aeropuerto/controller/FXController/TerminalFXController.java
+++ src/main/java/com/aeropuerto/webapp/Aeropuerto/controller/FXController/TerminalFXController.java
@@ -5,11 +5,28 @@ import java.util.ResourceBundle;
 
 import com.aeropuerto.webapp.Aeropuerto.system.Main;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import lombok.Setter;
 
 public class TerminalFXController implements Initializable{
 
+
+    @FXML
+    Button btnBuscar, btnRegresar, btnAgregar, btnEditar, btnEliminar, btnVaciar;
+    @FXML
+    TextField tfBuscar, tfId, tfNumeroDeTerminal, tfCapacidad, tfServiciosDisponibles;
+    @FXML
+    TableColumn colId, colNumeroDeTerminal, colCapacidad, colServiciosDisponibles, colAereopuerto;
+    @FXML
+    ComboBox cmbAereopuerto;
+    @FXML
+    TableView tblTerminales;
     @Setter
     private Main stage;
 
--- src/main/java/com/aeropuerto/webapp/Aeropuerto/model/Terminal.java
+++ src/main/java/com/aeropuerto/webapp/Aeropuerto/model/Terminal.java
@@ -25,3 +25,4 @@ public class Terminal {
     @ManyToOne(fetch = FetchType.EAGER)
     private Aeropuerto aeropuerto;
 }
+
